65d3bcec2ac81780e1a5176cd106dce1684f44b4
Moises19 <moihyo19@gmail.com>
2026-07-30 11:56:03 -0500

    Release v1.0.0 - Psicolife Landing Page

--- js/main.js
+++ js/main.js
@@ -66,3 +66,35 @@ window.addEventListener("resize", () => {
     });
 
 });
+
+/*========================================
+    Back To Top
+========================================*/
+
+const backToTop = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 400) {
+
+        backToTop.classList.add('show');
+
+    } else {
+
+        backToTop.classList.remove('show');
+
+    }
+
+});
+
+backToTop.addEventListener('click', () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: 'smooth'
+
+    });
+
+});
